3aef4b668e330e707f187478eadc620a5e96482a
Gustavo Andres Romero Ordoñez <gus.romero.ordonez@outlook.com>
2022-12-29 15:54:13 -0500

    I've added a class to shorten the url

---
+++ hey-url-challenge-code-dotnet/Util/ShortUrl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace hey_url_challenge_code_dotnet.Util
+{
+    public static class ShortUrl
+    {
+        private const string CharacterPool = "ABCDEFGHIJKLMNPQRSTUVWXYZ";
+        private static Random Random = new();
+        private static readonly object ThreadLock = new();
+
+        public static string Generated(int length)
+        {
+            StringBuilder poolBuilder = new(CharacterPool);
+            string pool = poolBuilder.ToString();
+            char[] shortUlr = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                lock (ThreadLock)
+                {
+                    int charIndex = Random.Next(0, pool.Length);
+                    shortUlr[i] = pool[charIndex];
+                }
+            }
+
+            return new string(shortUlr);
+        }
+    }
+}
