36a547c81f1123e036465ca41f7d2c40fa4bf118
Gustavo Andres Romero Ordoñez <gus.romero.ordonez@outlook.com>
2023-01-03 01:02:26 -0500

    I've developed a mock custom class to the unit test project

---
+++ tests/CustomMock.cs
@@ -0,0 +1,55 @@
+﻿using hey_url_challenge_code_dotnet.Rules.Contracts;
+using HeyUrlChallengeCodeDotnet.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shyjus.BrowserDetection;
+using System;
+
+namespace tests
+{
+    internal class CustomMock
+    {
+        public static HttpContext GetHttpContext()
+        {
+            Mock<HttpContext> httpContextMock = new();
+            Mock<HttpRequest> httpRequestMock = new();
+            httpRequestMock.Setup(req => req.Scheme).Returns("https");
+            httpRequestMock.Setup(req => req.Host).Returns(new HostString("localhost:44327"));
+            httpContextMock.Setup(ctx => ctx.Request).Returns(httpRequestMock.Object);
+
+            return httpContextMock.Object;
+        }
+
+        public static ITempDataDictionary GetTempData()
+        {
+            ITempDataProvider tempDataProvider = Mock.Of<ITempDataProvider>();
+            TempDataDictionaryFactory tempDataDictionaryFactory = new(tempDataProvider);
+            ITempDataDictionary tempData = tempDataDictionaryFactory.GetTempData(new DefaultHttpContext());
+
+            return tempData;
+        }
+
+        public static ILogger<UrlsController> GetUrlLogger()
+        {
+            Mock<ILogger<UrlsController>> loggerMock = new();
+
+            return loggerMock.Object;
+        }
+
+        public static IBrowserDetector GetBrowserDetector()
+        {
+            Mock<IBrowserDetector> browserDetectorMock = new();
+
+            return browserDetectorMock.Object;
+        }
+
+        public static Lazy<IUrlService> GetLazyUrlService()
+        {
+            Mock<IUrlService> urlServiceMock= new();
+
+            return new Lazy<IUrlService>(() => urlServiceMock.Object);
+        }
+    }
+}
--- tests/UrlsControllerTest.cs
+++ tests/UrlsControllerTest.cs
@@ -1,18 +1,34 @@
+using hey_url_challenge_code_dotnet.Rules.Contracts;
+using HeyUrlChallengeCodeDotnet.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
 using NUnit.Framework;
+using Shyjus.BrowserDetection;
+using System;
 
 namespace tests
 {
     public class UrlsControllerTest
     {
+        private UrlsController Controller { get; set; }
+
         [SetUp]
         public void Setup()
         {
+            this.Controller = new UrlsController(CustomMock.GetUrlLogger(), CustomMock.GetBrowserDetector(), CustomMock.GetLazyUrlService());
+            this.Controller.ControllerContext.HttpContext = CustomMock.GetHttpContext();
+            this.Controller.TempData = CustomMock.GetTempData();
         }
 
         [Test]
         public void TestIndex()
         {
-            Assert.Pass();
+            IActionResult result = this.Controller.Index();
+            Assert.IsInstanceOf<ViewResult>(result);
+            ViewResult viewResult = result as ViewResult;
+            Assert.AreEqual(null, viewResult.ViewName);
         }
     }
 }
